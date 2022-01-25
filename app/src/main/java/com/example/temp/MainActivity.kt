c1e6d589523b30798587f520295360c88d52eb4f
Admin <kirolosa4@gmail.com>
2022-01-25 15:40:47 +0200

    Revert "development commit 2"
    This reverts commit a305f332a9ba10e035cd5531948c63067d993d4a.

--- app/src/main/java/com/example/temp/MainActivity.kt
+++ app/src/main/java/com/example/temp/MainActivity.kt
@@ -8,6 +8,6 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        //first branch 1 commit
+        //development branch 2 commit
     }
 }
