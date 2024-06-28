bdb4177689dcf43fb56d0b6a4d078ed33f26072c
george <george.samir.mansour@gmail.com>
2024-06-28 18:36:24 +0300

    finished more screen

---
+++ core/lib/dto/models/address/address_request.g.dart
@@ -0,0 +1,33 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'address_request.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+AddressRequest _$AddressRequestFromJson(Map<String, dynamic> json) =>
+    AddressRequest(
+      clientId: (json['client_id'] as num).toInt(),
+      street: json['street'] as String,
+      street2: json['street2'] as String,
+      city: json['city'] as String,
+      longitude: (json['partner_longitude'] as num).toDouble(),
+      latitude: (json['partner_latitude'] as num).toDouble(),
+      countryId: (json['country_id'] as num).toInt(),
+      stateId: (json['state_id'] as num).toInt(),
+      zip: json['zip'] as String,
+    );
+
+Map<String, dynamic> _$AddressRequestToJson(AddressRequest instance) =>
+    <String, dynamic>{
+      'client_id': instance.clientId,
+      'street': instance.street,
+      'street2': instance.street2,
+      'city': instance.city,
+      'country_id': instance.countryId,
+      'state_id': instance.stateId,
+      'zip': instance.zip,
+      'partner_latitude': instance.latitude,
+      'partner_longitude': instance.longitude,
+    };
