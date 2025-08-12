d57f979b0f163d9c8383ca206ef28132da075ce6
Dokkan-Dev <158779921+Dokkan-Dev@users.noreply.github.com>
2025-08-13 01:14:16 +0300

    adding notification functionality

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,12 @@
 import 'dart:async';
+import 'dart:math';
 import 'package:deel/deel.dart';
 import 'package:firebase_crashlytics/firebase_crashlytics.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_paymob/flutter_paymob.dart';
+import 'package:permission_handler/permission_handler.dart';
 import 'package:provider/provider.dart';
 import 'package:simple_shared_pref/simple_shared_pref.dart';
 
@@ -16,35 +20,90 @@ import 'flavors.dart';
 import 'my_app.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
+
 // late ObjectBox objectBox;
 FutureOr<void> main() async {
   /// ensure widget init
   WidgetsFlutterBinding.ensureInitialized();
 
-  await  FlutterPaymob.instance.initialize(
+  await FlutterPaymob.instance.initialize(
       userTokenExpiration: 3600, // optional, default is 30 days
       apiKey:
-      "ZXlKaGJHY2lPaUpJVXpVeE1pSXNJblI1Y0NJNklrcFhWQ0o5LmV5SmpiR0Z6Y3lJNklrMWxjbU5vWVc1MElpd2ljSEp2Wm1sc1pWOXdheUk2TVRBME9ETTJNaXdpYm1GdFpTSTZJbWx1YVhScFlXd2lmUS5nNnIyd09hek1naVM2RUQxQWxITWRmbl9zOFUwOEowRmtDZTdnRndfMGlGb3F1TERDRVJVNThBd0l5dWZJZ1B3QVd5aVlVYlQtcG9nVjVlQU8wSmxBUQ==", //  // from dashboard Select Settings -> Account Info -> API Key
-      integrationID: 5106629 ,
-      walletIntegrationId: 5106875 ,
+          "ZXlKaGJHY2lPaUpJVXpVeE1pSXNJblI1Y0NJNklrcFhWQ0o5LmV5SmpiR0Z6Y3lJNklrMWxjbU5vWVc1MElpd2ljSEp2Wm1sc1pWOXdheUk2TVRBME9ETTJNaXdpYm1GdFpTSTZJbWx1YVhScFlXd2lmUS5nNnIyd09hek1naVM2RUQxQWxITWRmbl9zOFUwOEowRmtDZTdnRndfMGlGb3F1TERDRVJVNThBd0l5dWZJZ1B3QVd5aVlVYlQtcG9nVjVlQU8wSmxBUQ==", //  // from dashboard Select Settings -> Account Info -> API Key
+      integrationID: 5106629,
+      walletIntegrationId: 5106875,
       iFrameID: 926227);
+
   ///
   await Firebase.initializeApp(
-      options: DefaultFirebaseOptions.currentPlatform,
+    options: DefaultFirebaseOptions.currentPlatform,
   );
 
+  await FirebaseMessaging.instance.setAutoInitEnabled(true);
+  // You may set the permission requests to "provisional" which allows the user to choose what type
+  // of notifications they would like to receive once the user receives a notification.
 
+  // For apple platforms, ensure the APNS token is available before making any FCM plugin API calls
+  final apnsToken = await FirebaseMessaging.instance.getAPNSToken();
+  if (apnsToken != null) {
+    // APNS token is available, make FCM plugin API requests...
+  }
+
+  final notificationSettings =
+      await FirebaseMessaging.instance.requestPermission(
+    provisional: true,
+    alert: true,
+    badge: true,
+    sound: true,
+  );
+
+  // Android 13+: Request permission
+  // if (defaultTargetPlatform == TargetPlatform.android) {
+  //   if (await Permission.notification.isDenied ||
+  //       await Permission.notification.isPermanentlyDenied) {
+  //     await Permission.notification.request();
+  //   }
+  // }
+
+  await requestNotificationPermissions();
+
+  String? token = await FirebaseMessaging.instance.getToken();
+  if (token != null) {
+    LoggerModule.log(
+        message: "********* fcm Token $token **********", name: "fcm Token");
+    // Send initial token to your server
+  }
+
+  FirebaseMessaging.instance.onTokenRefresh.listen((fcmToken) {
+    // print("********* fcm Token $fcmToken **********");
+    LoggerModule.log(
+        message: "********* fcm Token $fcmToken **********", name: "fcm Token");
+  }).onError((err) {
+    // print("********* error getting device fcm Token **********");
+    LoggerModule.log(
+        message: "********* error getting device fcm Token **********",
+        name: "fcm Token");
+  });
+
+  if (notificationSettings.authorizationStatus ==
+      AuthorizationStatus.authorized) {
+    debugPrint('✅ Notifications authorized');
+  } else if (notificationSettings.authorizationStatus ==
+      AuthorizationStatus.provisional) {
+    debugPrint('⚠️ Notifications provisionally authorized');
+  } else {
+    debugPrint('❌ Notifications denied');
+  }
 
   F.appFlavor ??= Flavor.app_stage;
 
-  if( F.apiUrl.isEmpty){
+  if (F.apiUrl.isEmpty) {
     F.apiUrl = 'https://dokkan.odoo.com/';
   }
 
-  if(F.adminApiUrl.isEmpty){
+  if (F.adminApiUrl.isEmpty) {
     F.adminApiUrl = 'https://adminapi.deel-app.com/api/';
   }
-
 
   // if (kDebugMode) {
   //   // Force disable Crashlytics collection while doing every day development.
@@ -73,6 +132,7 @@ FutureOr<void> main() async {
   // ChuckerFlutter.showOnRelease = true;
   HttpOverrides.global = AppHttpOverrides();
   await DependencyInjectionService().init();
+
   /// run app and use provider for app config
   runApp(MultiProvider(providers: [
     ChangeNotifierProvider<AppProviderModule>(create: (_) {
@@ -80,8 +140,20 @@ FutureOr<void> main() async {
       appProviderModule.initAppThemeAndLanguage();
       return appProviderModule;
     }),
-  ], child: const   MyApp()));
+  ], child: const MyApp()));
   // _runAppWithSentry();
+}
+
+Future<void> requestNotificationPermissions() async {
+  final PermissionStatus status = await Permission.notification.request();
+  if (status.isGranted) {
+    // Notification permissions granted
+  } else if (status.isDenied) {
+    // Notification permissions denied
+  } else if (status.isPermanentlyDenied) {
+    // Notification permissions permanently denied, open app settings
+    await openAppSettings();
+  }
 }
 
 void _initAdminDio() {
