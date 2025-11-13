9a92700a14494be750c2aaf8d29c72972c825467
Dokkan-Dev <158779921+Dokkan-Dev@users.noreply.github.com>
2025-11-13 23:14:55 +0200

    incrementing app version number

--- lib/features/more/more/ui/more_page.dart
+++ lib/features/more/more/ui/more_page.dart
@@ -32,7 +32,7 @@ class MorePage extends BaseStatefulWidget {
 }
 
 class _MoreWidgetState extends BaseState<MorePage> {
-  final deelVersionNumber = "0.1.13";
+  final deelVersionNumber = "0.1.14";
 
   @override
   void initState() {
