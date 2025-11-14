5b78e6ff340d9f6c7b22be53b161e5543730f9cb
Dokkan-Dev <158779921+Dokkan-Dev@users.noreply.github.com>
2025-11-14 20:04:14 +0200

    reset company type after registration

--- lib/features/authentication/newAccount/ui/new_account_bloc.dart
+++ lib/features/authentication/newAccount/ui/new_account_bloc.dart
@@ -27,6 +27,7 @@ class NewAccountBloc extends BlocBase {
     streetNameBloc.textFormFiledBehaviour.sink.add(TextEditingController());
     neighborhoodBloc.textFormFiledBehaviour.sink.add(TextEditingController());
     cityBloc.textFormFiledBehaviour.sink.add(TextEditingController());
+    companyBloc.textFormFiledBehaviour.sink.add(TextEditingController());
     passwordBloc.textFormFiledBehaviour.sink.add(TextEditingController());
     confirmPasswordBloc.textFormFiledBehaviour.sink
         .add(TextEditingController());
@@ -43,12 +44,11 @@ class NewAccountBloc extends BlocBase {
       },
     );
 
-    CompanyTypeRemote()
-        .callApiAsStream().listen((event) {
-          _companyTypeBehaviour.sink.add(event);
-        },);
-
-
+    CompanyTypeRemote().callApiAsStream().listen(
+      (event) {
+        _companyTypeBehaviour.sink.add(event);
+      },
+    );
   }
 
   final TextFormFiledBloc fullNameBloc = TextFormFiledBloc();
@@ -70,7 +70,7 @@ class NewAccountBloc extends BlocBase {
       BehaviorSubject();
 
   final BehaviorSubject<ApiState<List<DropDownMapper>>> _companyTypeBehaviour =
-  BehaviorSubject();
+      BehaviorSubject();
 
   DropDownMapper? selectedState;
   DropDownMapper? selectedCompany;
@@ -95,7 +95,7 @@ class NewAccountBloc extends BlocBase {
       fullNameBloc.stringStream,
       facilityNameBloc.stringStream,
       companyBloc.stringStream,
-      (fullName, platformName,companyType) => isNamesInfoValid);
+      (fullName, platformName, companyType) => isNamesInfoValid);
 
   Stream<bool> get validateLocationStream => Rx.combineLatest3(
       streetNameBloc.stringStream,
@@ -116,7 +116,8 @@ class NewAccountBloc extends BlocBase {
 
   bool get isNamesInfoValid =>
       _validatorModule.isFiledNotEmpty(fullNameBloc.value) &&
-      _validatorModule.isFiledNotEmpty(facilityNameBloc.value) && selectedCompany!=null;
+      _validatorModule.isFiledNotEmpty(facilityNameBloc.value) &&
+      selectedCompany != null;
 
   bool get isLocationValid =>
       _validatorModule.isFiledNotEmpty(neighborhoodBloc.value) &&
@@ -130,17 +131,15 @@ class NewAccountBloc extends BlocBase {
           passwordBloc.value, confirmPasswordBloc.value);
 
   Stream<ApiState<LoginMapper>> get register => RegisterRemote(
-          shopName: facilityNameBloc.value,
-          name: fullNameBloc.value,
-          phone: "${this.countryCode}${mobileNumber}",
-          password: passwordBloc.value,
-          latitude: _longitudeBehaviour.valueOrNull.toString(),
-          longitude: _latitudeBehaviour.valueOrNull.toString(),
-          countryId: _countryId,
-          companyId: selectedCompany != null ? int.parse(selectedCompany!.id) : 0,
-  )
-      .callApiAsStream();
-
+        shopName: facilityNameBloc.value,
+        name: fullNameBloc.value,
+        phone: "${this.countryCode}${mobileNumber}",
+        password: passwordBloc.value,
+        latitude: _longitudeBehaviour.valueOrNull.toString(),
+        longitude: _latitudeBehaviour.valueOrNull.toString(),
+        countryId: _countryId,
+        companyId: selectedCompany != null ? int.parse(selectedCompany!.id) : 0,
+      ).callApiAsStream();
 
   Stream<ApiState<List<DropDownMapper>>> get companyStream =>
       _companyTypeBehaviour.stream;
@@ -167,11 +166,11 @@ class NewAccountBloc extends BlocBase {
     var client = http.Client();
     final Map<String, String> _headers = const {
       "User-Agent":
-      "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/132.0.0.0 Safari/537.36"
+          "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/132.0.0.0 Safari/537.36"
     };
     String url =
         'https://nominatim.openstreetmap.org/reverse?format=json&lat=${_latitudeBehaviour.value}&lon=${_longitudeBehaviour.value}&zoom=18&addressdetails=1';
-    var response = await client.get(Uri.parse(url),headers: _headers);
+    var response = await client.get(Uri.parse(url), headers: _headers);
     var decodedResponse =
         jsonDecode(utf8.decode(response.bodyBytes)) as Map<dynamic, dynamic>;
     Map<dynamic, dynamic> address = decodedResponse['address'];
@@ -179,7 +178,6 @@ class NewAccountBloc extends BlocBase {
     _setNeighborhood(address);
 
     String area = Apputils.getFormattedAddress(address);
-
 
     _setStreamName(area);
   }
