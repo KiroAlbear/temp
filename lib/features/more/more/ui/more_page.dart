0ceb9e1df858093271570c68d1246921306b4a78
Dokkan-Dev <158779921+Dokkan-Dev@users.noreply.github.com>
2025-11-16 23:42:11 +0200

    add otp testing code

--- lib/features/more/more/ui/more_page.dart
+++ lib/features/more/more/ui/more_page.dart
@@ -32,7 +32,7 @@ class MorePage extends BaseStatefulWidget {
 }
 
 class _MoreWidgetState extends BaseState<MorePage> {
-  final deelVersionNumber = "0.1.16";
+  final deelVersionNumber = "0.1.17";
 
   @override
   void initState() {
