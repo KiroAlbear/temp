9a8837089e0c9a8c8bed33ff20a92c70bb1a78ef
Dokkan-Dev <158779921+Dokkan-Dev@users.noreply.github.com>
2025-12-13 22:40:15 +0200

    updating version number

--- lib/features/more/more/ui/more_page.dart
+++ lib/features/more/more/ui/more_page.dart
@@ -32,7 +32,7 @@ class MorePage extends BaseStatefulWidget {
 }
 
 class _MoreWidgetState extends BaseState<MorePage> {
-  final deelVersionNumber = "0.1.24";
+  final deelVersionNumber = "0.1.25";
 
   @override
   void initState() {
