e4c35051d0eb244a75b14792b9327784318dcb67
Dokkan-Dev <158779921+Dokkan-Dev@users.noreply.github.com>
2026-01-03 14:05:18 +0200

    fix company_type_reeponse_model.dart

--- lib/core/dto/models/register/company_type_reeponse_model.dart
+++ lib/core/dto/models/register/company_type_reeponse_model.dart
@@ -1,25 +1,22 @@
-
 class CompanyTypeResponseModel {
   int? id;
   String? name;
   String? type;
   bool? active;
   String? displayName;
-  List<int>? createUid;
+  // List<int>? createUid;
   String? createDate;
-  List<int>? writeUid;
+  // List<int>? writeUid;
   String? writeDate;
 
   CompanyTypeResponseModel(
       {this.id,
-        this.name,
-        this.type,
-        this.active,
-        this.displayName,
-        this.createUid,
-        this.createDate,
-        this.writeUid,
-        this.writeDate});
+      this.name,
+      this.type,
+      this.active,
+      this.displayName,
+      this.createDate,
+      this.writeDate});
 
   CompanyTypeResponseModel.fromJson(Map<String, dynamic> json) {
     id = json['id'];
@@ -27,9 +24,7 @@ class CompanyTypeResponseModel {
     type = json['type'];
     active = json['active'];
     displayName = json['display_name'];
-    createUid = json['create_uid'].cast<int>();
     createDate = json['create_date'];
-    writeUid = json['write_uid'].cast<int>();
     writeDate = json['write_date'];
   }
 
@@ -40,9 +35,7 @@ class CompanyTypeResponseModel {
     data['type'] = this.type;
     data['active'] = this.active;
     data['display_name'] = this.displayName;
-    data['create_uid'] = this.createUid;
     data['create_date'] = this.createDate;
-    data['write_uid'] = this.writeUid;
     data['write_date'] = this.writeDate;
     return data;
   }
