cb72557d8bb8c04bc73393b67787f61c16379ae0
Dokkan-Dev <158779921+Dokkan-Dev@users.noreply.github.com>
2026-01-28 22:48:08 +0200

    fix offers list height

--- lib/features/home/home/ui/offers_listing_widget.dart
+++ lib/features/home/home/ui/offers_listing_widget.dart
@@ -44,40 +44,47 @@ class _OffersListingWidgetState extends State<OffersListingWidget>
   Widget _buildWidget(List<OfferMapper> list) => list.isEmpty
       ? SizedBox()
       : Column(
-        children: [
-          SizedBox(height: 20.h),
-           Padding(
-              padding: EdgeInsets.symmetric(horizontal: 16.w),
-              child: CustomText(
-                text: S.of(context).lastOffers,
-                customTextStyle: BoldStyle(color: secondaryColor, fontSize: 20.sp),
-              ),
-            ),
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            widget.isMainPage ? SizedBox() : SizedBox(height: 20.h),
+            widget.isMainPage
+                ? SizedBox()
+                : Padding(
+                    padding: EdgeInsets.symmetric(horizontal: 16.w),
+                    child: CustomText(
+                      text: S.of(context).lastOffers,
+                      customTextStyle: BoldStyle(
+                        color: secondaryColor,
+                        fontSize: 20.sp,
+                      ),
+                    ),
+                  ),
+            widget.isMainPage ? SizedBox() : SizedBox(height: 20.h),
+            widget.isMainPage
+                ? Expanded(child: _buildOfferList(list))
+                : SizedBox(height: 120.h, child: _buildOfferList(list)),
+          ],
+        );
 
-          SizedBox(
-              height: widget.isMainPage ? 100.h : 120.h,
-              child: ListView.separated(
-                shrinkWrap: true,
-                scrollDirection: widget.isMainPage
-                    ? Axis.vertical
-                    : Axis.horizontal,
-                // physics: const PageScrollPhysics(),
-                // controller: _pageScrollController,
-                padding: EdgeInsets.symmetric(horizontal: 16.w),
-                itemBuilder: (context, index) => OfferItem(
-                  isInProductPage: false,
-                  isClickable: list[index].link.toLowerCase().trim() != "nolink",
-                  item: list[index],
-                  homeBloc: widget.homeBloc,
-                  // index: index
-                ),
-                separatorBuilder: (context, index) => SizedBox(
-                  width: widget.isMainPage == false ? 12.w : null,
-                  height: widget.isMainPage == true ? 20.h : null,
-                ),
-                itemCount: list.length,
-              ),
-            ),
-        ],
-      );
+  ListView _buildOfferList(List<OfferMapper> list) {
+    return ListView.separated(
+      shrinkWrap: true,
+      scrollDirection: widget.isMainPage ? Axis.vertical : Axis.horizontal,
+      // physics: const PageScrollPhysics(),
+      // controller: _pageScrollController,
+      padding: EdgeInsets.symmetric(horizontal: 16.w),
+      itemBuilder: (context, index) => OfferItem(
+        isInProductPage: false,
+        isClickable: list[index].link.toLowerCase().trim() != "nolink",
+        item: list[index],
+        homeBloc: widget.homeBloc,
+        // index: index
+      ),
+      separatorBuilder: (context, index) => SizedBox(
+        width: widget.isMainPage == false ? 12.w : null,
+        height: widget.isMainPage == true ? 20.h : null,
+      ),
+      itemCount: list.length,
+    );
+  }
 }
